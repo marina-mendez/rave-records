c459bfd89cfe9bec10391e37a68bdc14783ecb0b
Marina Mendez <marina99mendez@gmail.com>
2024-02-29 19:49:53 -0300

    style.css added to public

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <!-- style.css-->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
 </head>
 <body>
