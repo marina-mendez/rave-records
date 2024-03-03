ba911a74bf054e13bdfea86022f184d3422819de
Eugenia Toledo <m.eugenia.toledo@hotmail.com>
2024-03-02 22:16:31 -0300

    added clases section & new styles

--- resources/views/clases/clases.blade.php
+++ resources/views/clases/clases.blade.php
@@ -4,26 +4,39 @@
 
 @section('content')
 <section>
-    <section>
+    <section class="clases-span">
         <article>
-            <span>Clases de DJ</span>
+            <span class="first-span bold">Clases de DJ</span>
             <span>Dj Profesional</span>
             <span>Nivel 1</span>
         </article>
-        <article>
-            <a href="" class="">Reservar ahora</a>
+        <article class="clases-btn">
+            <a href="" class="btn-reservar">Reservar ahora</a>
         </article>
     </section>
-    <section class="container-registro">
-        <section>
-            <h3>Iniciate como DJ Profesional</h3>
+    <hr>
+    <section class="clases-fsection">
+        <section class="text-fsection">
+            <h3 class="bold">Iniciate como DJ Profesional</h3>
             <span>Comenzamos el 13 de Noviembre</span>
             <p>Contamos con un equipo acustizado y acondicionado con el mejor equipamiento del mercado.</p>
             <p> Setup Piooner CDJ 3000 - CDJ 2000 NXS - Reloop RP8000 MK2 - Xone 96 - AIAIAI Dj Preset.</p>
-            <span>Precio $30.000.-</span>
+            <span class="bold">Precio $30.000.-</span>
         </section>
-        <section>
+        <section class="image-fsection">
             <img src="./images/clases.jpg" alt="">
+        </section>
+    </section>
+    <section class="clases-ssection">
+        <section class="text-ssection">
+            <h3>Preguntas frecuentes</h3>
+            <p>-Los cursos son máximo de 4 alumnos por curso.</p>
+            <p>-No hace falta que los alumnos traigan nada.</p>
+            <p>-Las clases no se pueden recuperar.</p>
+            <p>-El precio del curso es por mes y la duración total es de 2 meses.</p>
+        </section>
+        <section class="image-ssection">
+            <img src="./images/alumno.jpg" alt="">
         </section>
     </section>
 </section>
